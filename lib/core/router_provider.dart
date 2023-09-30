4f2e4c52a62bf48798f393cda2803e67adaafaf2
tsubasa1997 <96885280+tsubasa1997@users.noreply.github.com>
2023-10-01 04:56:32 +0900

    いらなくなったファイルを削除

--- lib/core/router_provider.dart
+++ lib/core/router_provider.dart
@@ -4,7 +4,6 @@ import 'package:go_router/go_router.dart';
 import 'package:hackathon/core/exceptions/invalid_route_arg_exception.dart';
 import 'package:hackathon/features/auth/data/auth_repository.dart';
 import 'package:hackathon/features/auth/view/sign_in_page.dart';
-import 'package:hackathon/features/compass/compass_screen.dart';
 import 'package:hackathon/features/home/home_page.dart';
 import 'package:hackathon/features/my_page/my_page.dart';
 import 'package:hackathon/features/sample/sample_next_page.dart';
@@ -14,7 +13,7 @@ import 'package:hackathon/features/time_line/time_line_screen.dart';
 final routerProvider = Provider((ref) {
   return GoRouter(
     debugLogDiagnostics: true,
-    initialLocation: CompassScreen.routeName,
+    initialLocation: '/${HomePage.routeName}',
     redirect: (context, state) async {
       // print(state);
       // return ref.read(authStateProvider).when(
@@ -72,11 +71,6 @@ final routerProvider = Provider((ref) {
             path: TimeLineScreen.routeName,
             name: TimeLineScreen.routeName,
             builder: (_, __) => const TimeLineScreen(),
-          ),
-          GoRoute(
-            path: CompassScreen.routeName,
-            name: CompassScreen.routeName,
-            builder: (_, __) => const CompassScreen(),
           ),
         ],
       ),
