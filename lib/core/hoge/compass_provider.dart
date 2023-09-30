f16e36d14bb74477c9f87a2f0077c1632144d8a4
Shoryu Yamamoto <tatsubee@pixiv.co.jp>
2023-09-30 22:04:19 +0900

    Update compass_provider.dart

--- lib/core/hoge/compass_provider.dart
+++ lib/core/hoge/compass_provider.dart
@@ -1,11 +1,6 @@
 import 'package:flutter_compass/flutter_compass.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final compassProvider = StreamProvider((ref) async* {
-  final events = FlutterCompass.events;
-  if (events == null) {
-    yield null;
-  }
-  final last = await events?.last;
-  yield last;
-});
+final compassProvider = StreamProvider.autoDispose(
+  (ref) => FlutterCompass.events ?? Stream.value(null),
+);
