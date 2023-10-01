36de7ead402ac269decd6f3793bd63cbb2f5af0a
Shoryu Yamamoto <tatsubee@pixiv.co.jp>
2023-10-01 13:28:13 +0900

    upload images

--- lib/features/panorama/filter_controller/filter_controller.dart
+++ lib/features/panorama/filter_controller/filter_controller.dart
@@ -2,10 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 enum Filter {
-  current('現在', 'assets/images/panorama_sample.jpg'),
-  past('昔', 'hagehage'),
-  future('近未来', 'pogepoge'),
-  fantasy('異世界', 'chogechoge'),
+  current('現在', 'assets/images/current.png'),
+  past('昔', 'assets/images/past.png'),
+  future('近未来', 'assets/images/future.png'),
+  fantasy('異世界', 'assets/images/fantasy.png'),
   ;
 
   const Filter(this.text, this.imageRef);
